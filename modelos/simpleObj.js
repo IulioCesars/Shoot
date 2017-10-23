c0fcb69927bd34c10946e2109b47b9401c343b3c
Iulio Cesar <iuliocesars@gmail.com>
2017-10-23 14:45:16 -0500

    Add files via upload

--- modelos/simpleObj.js
+++ modelos/simpleObj.js
@@ -1,5 +1,19 @@
-class simpleObj extends OBJLoader{
-  constructor() {
+class simpleObj extends THREE.MTLLoader{
+  
+  constructor(path, obj, scene) {
     super();
+    this.setBaseUrl( path );
+			this.setPath( path );
+			this.load( obj + ".mtl", function( materials ) {
+					materials.preload();
+					var objLoader = new THREE.OBJLoader();
+					objLoader.setMaterials( materials );
+					objLoader.setPath( path );
+					objLoader.load( obj + ".obj", function ( object ) {
+							scene.add(object);
+					}, null, null );
+			});
+    
   }
+  
 }
